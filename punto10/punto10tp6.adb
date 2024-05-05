a59ef56b1eab6b4aedbd05adae2053bda630dace
chella16 <166649236+chella16@users.noreply.github.com>
2024-05-05 20:35:52 -0300

    Update punto10tp6.adb

--- punto10/punto10tp6.adb
+++ punto10/punto10tp6.adb
@@ -75,11 +75,15 @@ procedure Punto10tp6 is
       Longaux: Natural;
    begin
       Nombreaux:=Info(Listap).rname;
-      Longaux:= Info(Listap).rlong;
+      Longaux:= Info(Listap).Rlong;
       while Nombreaux(1..Longaux) /= nombre(1..long) or vacia(listap) loop
          Listap:= Sig(Listap);
          Nombreaux:=Info(Listap).Rname;
-         Longaux:= Info(Listap).Rlong; 
+         Longaux:= Info(Listap).Rlong;
+         if Vacia(Listap) then
+            Put(" la receta q buscas no existe men ");
+            New_Line;
+         end if;
       end loop;
    end Buscar_Receta;
       
@@ -119,13 +123,11 @@ procedure Punto10tp6 is
    end Buscar_Imprimir;
    
 
-   procedure impresionlista (Listap: in out Lista_postres.Tlista) is
-      Listauxp: Lista_Postres.tlista:= listap;
-      Listaing: Lista_Ingredientes.Tlista;
+   procedure impresionlista_ingredientes (Listaing: in out Lista_Ingredientes.Tlista) is
+      Listaingaux: Lista_Ingredientes.Tlista:= listaing;
    begin
-      Listaing:= Info(Listauxp).Ingredientes;
-      Imprimir(Listaing);
-   end impresionlista;
+      Imprimir(Listaingaux);
+   end impresionlista_ingredientes;
  
 
    procedure Insertar_Ingredientes (Listap: in out Lista_Postres.Tlista) is
@@ -136,7 +138,7 @@ procedure Punto10tp6 is
       Long:Natural;
       Listauxp: Lista_Postres.Tlista:= Listap;
    begin
-      Put(" cual receta queres modificar ");
+      Put(" cual receta queres modificar agregandole ingredientes ");
       Get_Line(Nombre, Long);
       Buscar_Receta(Listauxp, Nombre, Long);
       Put(" ingrese los ingredientes que quiere agregar a la receta ");
@@ -154,16 +156,81 @@ procedure Punto10tp6 is
       Insertar(Listap, Regipostre);
       if not vacia(listauxp) then
          Put(" la lista de ingredientes fue actualizada con exito ");
-         Impresionlista(Listap);
+         Impresionlista_ingredientes(Regipostre.Ingredientes);
       else
          Put(" la lista no fue actualizada con exito :( ");
       end if;
    end Insertar_Ingredientes;
          
+
+   procedure Eliminar_Ingrediente (Listap: in out Lista_Postres.Tlista) is
+      Listauxp: Lista_Postres.Tlista:= Listap;
+      Nombre: String20;
+      Long, Inglong, longaux:Natural;
+      Ingsup, nombreaux: String15;
+      Lista_Ing, Lista_Ingaux: Lista_Ingredientes.Tlista;
+      Regipostre: R_Postre;
+   begin
+      Put(" cual receta queres modificar eliminando algun ingrediente?... ");
+      Get_Line(Nombre, Long);
+      Skip_Line;
+      Buscar_Receta(Listauxp, Nombre, Long);
+      Regipostre:= Info(Listauxp);
+      Suprimir(Listap, Regipostre);
+      Lista_Ing:= Regipostre.Ingredientes;
+      Lista_Ingaux:= Regipostre.Ingredientes;
+      Put(" ingrese el ingrediente que quiere eliminar de la receta ");
+      Get_Line (Ingsup, Inglong);
+      Skip_Line;
+      Nombreaux:= Info(Lista_Ingaux).Ingname;
+      Longaux:= Info(Lista_Ingaux).Ilong;
+      while ingsup(1..inglong) /= Nombreaux(1..Longaux) or Vacia(Lista_ingaux) loop
+         Lista_ingaux:= Sig(Lista_ingaux);
+         Nombreaux:=Info(Lista_ingaux).ingname;
+         Longaux:= Info(Lista_ingaux).ilong;
+      end loop;
+      if Vacia(Lista_Ingaux) then
+         Put (" el ingrediente no esta en esta receta ");
+      else
+         Put( " eliminando el ingrediente... ");
+         New_Line;
+         Suprimir(Lista_Ing,Info(Lista_Ingaux));
+         Regipostre.Ingredientes:= Lista_Ing;
+         Insertar(Listap, Regipostre);
+         Put(" los ingredientes de la receta actualizada es: ");
+         Impresionlista_ingredientes(regipostre.ingredientes);
+         New_Line;
+      end if;
+   end Eliminar_Ingrediente;
+   
+
+   procedure Eliminar_Receta (Listap: in out Lista_Postres.Tlista) is
+      Listapaux: Lista_Postres.Tlista:=listap;
+      Nombreaux: String20;
+      Longaux: Natural;
+   begin
+      Put(" ingrese el nombre del postre que quiere eliminar ");
+      Get_Line(Nombreaux, Longaux);
+      Buscar_Receta (Listapaux, Nombreaux, Longaux);
+      if Vacia(Listapaux) then
+         Put (" t equivocaste de nombre ");
+      else
+         Put(" suprimiendo... ");
+         New_Line;
+         Suprimir( Listap, Info(Listapaux));
+         Put (" receta eliminada con exito ");
+      end if;
+   end Eliminar_Receta;
+
+
 begin
+   Crear_Postres (Listadepostres);
    Crear_Postres (Listadepostres);
    Buscar_Imprimir(Listadepostres);
    Insertar_Ingredientes(Listadepostres);
    Buscar_Imprimir(Listadepostres);
-   
+   Eliminar_Ingrediente(Listadepostres);
+   Buscar_Imprimir(Listadepostres);
+   Eliminar_Receta(Listadepostres);
+   Buscar_Imprimir(Listadepostres);
 end Punto10tp6;
