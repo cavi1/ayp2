a8357b47b210c8bf7690d7c324cc413b0e6abd87
cavi1 <52816119+cavi1@users.noreply.github.com>
2024-05-06 23:28:46 -0300

    Delete vecunipaqbas.ads

--- vecunipaqbas.ads
+++
@@ -1,11 +0,0 @@
-generic 
-   type Telemento is private;   type Tindice is (<>);
-   with procedure Put(X:in Telemento);
-   with procedure Get(X:out Telemento);
-   
-   package vecUniPaqBas is
-      type Tarreglo is array(Tindice) of Telemento;
-      procedure Leer(V:out Tarreglo);
-      procedure Escribir(V:in Tarreglo);
-   end vecUniPaqBas;
-      
